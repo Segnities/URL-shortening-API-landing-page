990d69901cdaf0e998858d80f90bd3cd56af7431
unknown <dimab787898@gmail.com>
2023-10-24 12:45:23 +0300

    auth
    auth before page loaded

--- frontend/components/AuthStateLayout.jsx
+++ frontend/components/AuthStateLayout.jsx
@@ -1,19 +1,36 @@
 import { useEffect } from 'react';
 import {onAuthStateChanged} from "firebase/auth";
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+
+import { setUserProvider } from '../store/reducer/userProvider';
 import {auth} from "../firebase";
+import { signUp } from '../store/reducer/auth';
 export default function AuthStateLayout({children}) {
+  const dispatch = useDispatch();
+
+
     useEffect(()=> {
       onAuthStateChanged(auth, (user)=> {
         if (user) {
           const currentUser = auth.currentUser;
           if (currentUser) {
+            const userProvider = axios.post("http://localhost:7886/api/user/auth", {
+              email: user.email,
+              providerId: user.providerId,
+              providerName: user.providerData[0].providerId
+            }).then(res => {
+              console.log(res);
+              dispatch(setUserProvider(res.data));
+              dispatch(signUp(JSON.stringify(currentUser)));
+            });
             console.log(currentUser);
             console.log(currentUser.email);
             console.log(currentUser.providerId);
             console.log(currentUser.providerData[0].providerId);
           }
         } else {
-          console.log('User logout');
+          console.log('User logout :(');
         }
       });
     }, []);
